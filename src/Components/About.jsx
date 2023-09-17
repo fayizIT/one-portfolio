69c7a54560a0fc0dc8e0d2ea335633f209a980cd
Fayiz <fayizcj94.com>
2023-09-17 20:48:00 +0530

    latest update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,44 +1,46 @@
-import React from 'react'
-import {MdOutlineKeyboardArrowRight} from "react-icons/md"
-import {Link} from 'react-scroll';
+import React from 'react';
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from 'react-scroll';
 
 const About = () => {
   return (
-    <div name="About" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
-        <div className='max-w-screen-lg pg-4 mx-auto flex flex-col justify-center w-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-grey-500'>
-                    About
-                </p>
-            </div>
-            <p className='text-xl mt-5'>
-            I have starting to Learn  in web development from Jan-2022 and completed Front end with React Js in Besant Technology Pvt Ltd, Where i get more explore about frontend and database technologies.
-Coding, its make me more interest to learn more technologies, and looking for the right place to gain strong knowledge on web development.
-My specialty is front-end web design and development, making pixel magic in web pages  and turning it into beautiful, semantic HTML,CSS,js,. My interests, however, extend beyond the web and I love helping people with branding. I even love making
-            </p>
-            <br/>
-
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-grey-500'>
-                    Education
-                </p>
-                </div>
-            <p>
-            I have completed BSc computer science in 2020 and I have 1.2 Years Experience in Technical support session In aabasoft pvt Kakkanad, In there am doing based on Network troubleshooting and find the packects loss  and resolved it.
-             This online portfolio is a good example of based on knowledge and skill in web development  
-            </p>
-            <div>
-               <Link  to="Contact"smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex item-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 to blue-500 cursor-pointer'>
-                        Know more
-                        <span className='hover:rotate-90 duration-300'>
-                        <MdOutlineKeyboardArrowRight size={25}
-                        className='ml-1'/>
-                        </span>
-                    </Link>
-               </div>
+    <div name="About" className='w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+      <div className='max-w-screen-lg mx-auto px-4 py-8 flex flex-col justify-center'>
+        <div className='pb-4'>
+          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+            About
+          </p>
         </div>
+        <p className='text-xl mt-5 mb-5'>
+          I started learning web development in Jan-2022 and completed Frontend with React Js at Besant Technology Pvt Ltd, where I gained extensive knowledge of frontend and database technologies. Coding has piqued my interest in learning more technologies, and I'm always looking for opportunities to strengthen my web development skills.
+
+          My specialty is front-end web design and development, creating pixel-perfect web pages with beautiful, semantic HTML, CSS, and JavaScript. My interests also extend to branding. I enjoy helping people with branding and design.
+
+          <br />
+        </p>
+
+        <div className='pb-4'>
+          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+            Education
+          </p>
+        </div>
+        <p>
+          I completed my BSc in computer science in 2020, and I have 1.2 years of experience in technical support at Aabasoft Pvt Ltd in Kakkanad. In this role, I focused on network troubleshooting and resolving packet loss issues.
+
+          This online portfolio is a testament to my knowledge and skills in web development.
+        </p>
+
+        <div>
+          <Link to="Contact" smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+            Know more
+            <span className='hover:rotate-90 duration-300'>
+              <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
+            </span>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default About;
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import html from '../assets/html.png';
 import css from '../assets/css.png';
 import javascript from '../assets/javascript.png';
@@ -11,111 +11,99 @@ import expressjs from '../assets/Expressjs.png';
 import Nodejs from '../assets/nodejs.png';
 import java from '../assets/java.png';
 
-
 const Skill = () => {
-
-    const techs =[
-        {
-            id:1,
-            src:html,
-            title: 'HTML',
-            Style:'shadow-orange-500'
-        },
-        {
-            id:2,
-            src:css,
-            title: 'CSS',
-            Style:'shadow-blue-500'
-        },
-        {
-            id:3,
-            src:javascript,
-            title: 'javascript',
-            Style:'shadow-yellow-500'
-        },
-        {
-            id:4,
-            src:bootstrap,
-            title: 'Bootstrap',
-            Style:'shadow-violet-500'
-        },
-        {
-            id:5,
-            src:java,
-            title: 'java',
-            Style:'shadow-blue'
-        },
-        {
-            id:6,
-            src:react,
-            title: 'React',
-            Style:'shadow-blue-600'
-        },
-        {
-            id:7,
-            src:github,
-            title: 'GitHub',
-            Style:'shadow-gray-400'
-        },
-        {
-            id:8,
-            src:CLanguage,
-            title: 'C Language',
-            Style:'shadow-blue'
-        },
-        {
-            id:9,
-            src:mongoDb,
-            title: 'MongoDB',
-            Style:'shadow-blue'
-        },
-        {
-            id:10,
-            src:expressjs,
-            title: 'Express Js',
-            Style:'shadow-blue'
-        },
-        {
-            id:11,
-            src:Nodejs,
-            title: 'Node Js',
-            Style:'shadow-blue'
-        },
-
-
-
-        
-        
-
-        
-
-       
-        
-    ]
-
+  const techs = [
+    {
+      id: 1,
+      src: html,
+      title: 'HTML',
+      style: 'shadow-orange-500',
+    },
+    {
+      id: 2,
+      src: css,
+      title: 'CSS',
+      style: 'shadow-blue-500',
+    },
+    {
+      id: 3,
+      src: javascript,
+      title: 'JavaScript',
+      style: 'shadow-yellow-500',
+    },
+    {
+      id: 4,
+      src: bootstrap,
+      title: 'Bootstrap',
+      style: 'shadow-violet-500',
+    },
+    {
+      id: 5,
+      src: react,
+      title: 'React',
+      style: 'shadow-blue-600',
+      
+    },
+    {
+      id: 6,
+      src: mongoDb,
+      title: 'MongoDB',
+      style: 'shadow-green-600',
+      
+    },
+    {
+        id: 7,
+          src: expressjs,
+        title: 'Express.js',
+        style: 'shadow-yellow-600',
+     
+      },
+    {
+      id: 8,
+        src: Nodejs,
+      title: 'Node.js',
+      style: 'shadow-green-600',
+    },
+    {
+      id: 10,
+    src: github,
+    title: 'GitHub',
+    style: 'shadow-gray-600',
     
+    },
+    {
+      id: 11,
+        src: CLanguage,
+      title: 'C Language',
+      style: 'shadow-blue-600'
+  
+    },
+    {
+        id: 11,
+    src: java,
+      title: 'Java',
+      style: 'shadow-red-400',
 
-                      
+    }
+    
+  ];
 
   return (
-    <div name='Skill' className='w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white'    >
-       <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
+    <div name='Skill' className='w-full bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
+      <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full text-white'>
         <div>
-            <p className='text-4xl font-bold border-b-4 border-black -500 p-2 inline'>Skill</p>
-            <p className='py-6'>These are the technologies Which I've worked with</p>
+          <p className='text-4xl font-bold border-b-4 border-black p-2 inline'>Skills</p>
+          <p className='py-6'>These are the technologies I've worked with</p>
         </div>
-        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
-            {techs.map(({ id,src,title, }) => (
-                
-                <div  key={id}
-                className={'shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${}'}>
-                <img src={src} alt="" className='w-20 mx-auto'/>
-                <p className='mt-4'>{title}</p>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 text-center py-8 px-4 sm:px-12'>
+          {techs.map(({ id, src, title, style }) => (
+            <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+              <img src={src} alt='' className='w-20 mx-auto' />
+              <p className='mt-4'>{title}</p>
             </div>
-            ))}
-           
+          ))}
         </div>
-       </div>
+      </div>
     </div>
   );
 };
